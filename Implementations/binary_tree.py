57bd2179bc5d47280360144d7f96578362bbc3e4
Tien Dinh <tiendinhphuc@gmail.com>
2019-02-18 23:15:58 -0600

    added midterm topics

--- Implementations/binary_tree.py
+++ Implementations/binary_tree.py
@@ -1,45 +1,45 @@
 # Binary Tree Implementation
 # We only deal with complete or nearly complete binary tree
-#			10
-#		9		 7		<= This is an example of a nearly complete BT
-#	 3	   5
+#           10
+#       9        7      <= This is an example of a nearly complete BT
+#     3    5
 #  0   0 1   1
 # This is NOT a binary search tree
 
 class BinaryTree:
 
-	def __init__(self, tree):
-		"""
-		Args:
-		tree: List representation of Binary Tree
-		"""
-		self.tree = tree
-		self.heap_size = len(tree)
+    def __init__(self, tree):
+        """
+        Args:
+        tree: List representation of Binary Tree
+        """
+        self.tree = tree
+        self.heap_size = len(tree)
 
-	def left_child(self, index):
-		return 2 * (index + 1)
+    def left_child(self, index):
+        return 2 * (index + 1)
 
-	def right_child(self, index):
-		return 2 * (index + 1) + 1
+    def right_child(self, index):
+        return 2 * (index + 1) + 1
 
-	def parent(self, index):
-		return (index + 1) // 2
+    def parent(self, index):
+        return (index + 1) // 2
 
-	def max_heapify(self, index):
-		largest = 0
-		left_index = self.left_child(index)
-		right_index = self.right_child(index)
-		if left_index <= self.heap_size and self.tree[left_index] > self.tree[index]:
-			largest = left_index
-		else:
-			largest = index
-		if right_index <= self.heap_size and self.tree[right_index] > self.tree[largest]:
-			largest = right_index
-		if largest != index:
-			self.swap(index, largest)
-			self.max_heapify(largest)
+    def max_heapify(self, index):
+        largest = 0
+        left_index = self.left_child(index)
+        right_index = self.right_child(index)
+        if left_index <= self.heap_size and self.tree[left_index] > self.tree[index]:
+            largest = left_index
+        else:
+            largest = index
+        if right_index <= self.heap_size and self.tree[right_index] > self.tree[largest]:
+            largest = right_index
+        if largest != index:
+            self.swap(index, largest)
+            self.max_heapify(largest)
 
-	def swap(self, index, largest):
-		tmp = self.tree[index]
-		self.tree[index] = self.tree[largest]
-		self.tree[largest] = tmp
+    def swap(self, index, largest):
+        tmp = self.tree[index]
+        self.tree[index] = self.tree[largest]
+        self.tree[largest] = tmp
---
+++ midterm-topics.md
@@ -0,0 +1,44 @@
+# Midterm 1 Topics
+## Tuesday, Feb 26, 2019
+ * Mathematics from prerequisite courses.
+ * What a recurrence is.
+ * How to solve recurrences in simple special cases.
+ * Formal definitions of O, Θ, Ω.
+ * How to prove that an expression is in O(e), Θ(e), Ω(e).
+ * What a divide and conquer algorithm is.
+ * The parts of a divide and conquer algorithm: divide, conquer, combine.
+ * How the Python function mergesort works, and its run time.
+ * How the run time of a divide and conquer algorithm can be expressed as a recurrence.
+ * What the Master Theorem is.
+ * How to solve recurrences using the Master Theorem.
+ * What a loop invariant is.
+ * How to prove an algorithm correct using a loop invariant.
+ * The three parts of such a proof: initialization, maintenance, termination.
+ * What a backtracking algorithm is.
+ * The parts of a backtracking algorithm: base case, make a decision, explore the decision, unmake the decision.
+ * What continuation passing is.
+ * How the Python function permute works, and its run time.
+ * What a binary tree is.
+ * Binary tree terminology: child, edge (link), empty tree, height (in terms of edges), internal node, external (leaf) node, left child, node (vertex), right child, root, parent, etc.
+ * What a complete binary tree is.
+ * What a nearly complete binary tree is.
+ * How to represent a nearly complete binary tree as an array.
+ * How to use ∞ and −∞ in algorithms.
+ * What a max-heap is.
+ * What a min-heap is.
+ * What a priority queue is.
+ * How to implement the `DEQUEUE`, `ENQUEUE`, `IS-EMPTY`, and `IS-FULL` operations for priority queues using max-heaps.
+ * You must also understand how these algorithms work (from Cormen’s text), and their run times.
+```
+BUILD-MAX-HEAP  
+HEAP-EXTRACT-MAX  
+HEAP-INCREASE-KEY  
+HEAP-MAXIMUM  
+HEAPSORT  
+INSERTION-SORT  
+LEFT  
+MAX-HEAP-INSERT  
+MAX-HEAPIFY  
+PARENT  
+RIGHT
+```
